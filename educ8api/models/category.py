c6e47d6422f34e610b55cfec569c7f5f5034f821
Matthew Hedges <matthedges35@gmail.com>
2023-07-13 22:47:55 -0500

    category and language fixtures added

--- educ8api/models/category.py
+++ educ8api/models/category.py
@@ -6,3 +6,4 @@ class Category(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    language = models.ForeignKey("Language", on_delete=models.CASCADE)
--- educ8api/models/vocabulary.py
+++ educ8api/models/vocabulary.py
@@ -6,6 +6,6 @@ class Vocabulary(models.Model):
 
     term = models.CharField(max_length=50)
     definition = models.CharField(max_length=500)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    example = models.CharField(max_length=1000)
-    language = models.ForeignKey("Language", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
+    example = models.CharField(max_length=1000, null=False)
+    language = models.ForeignKey("Language", on_delete=models.CASCADE, null=False)
